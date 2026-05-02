8c5baa2af216e81685725a60e0471329d82a49d9
Prithviraj Singh <145451860+prithviraj-101@users.noreply.github.com>
2026-05-02 16:49:04 +0530

    Improve comments and type checks in DataTypes.js
    Clarified the explanation of null and updated type checks.

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -9,14 +9,15 @@ number
 bigint
 string =""
 bolean = true/false
-null => standalone value, variable is defined but value is not assign 
+null => standalone value, variable is defined but value is not assign (0 is also a value so we prefer null)
 undefined => you declare a variable but value is not assign yet 
 symbol => for uniqueness 
  */
 //console.log(typeof name)// n JavaScript, name is already a predefined 
 // global variable (in browsers).so we not use as local variable 
-console.log(typeof undefined) ; // undefined
-console.log(typeof null) // object 
+console.log(typeof "hitesh")  // strig 
+console.log(typeof undefined) ; // type of undefined is -undefined(is laso a type )
+console.log(typeof null) // type of null is - object 
 
 /*
 difference between undefined and null .
